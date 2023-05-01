9af5b7701f47ee4c1573b0c9e0084bb6e973e92b
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-05-01 12:53:45 -0500

    player choice created

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
+    ""
+  ).toUpperCase;
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+};
+
+startGameBtn.addEventListener("click", function () {
+  console.log("Strating game...");
+  const playerSelection = getPlayerChoice;
+  console.log(playerSelection);
+});
