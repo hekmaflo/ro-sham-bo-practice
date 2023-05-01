5c2193c44fac36fdb53546da67fbcc3fe3d0a484
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-05-01 13:49:30 -0500

    winner created

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER WINS";
 
 let gameIsRunning = false;
 
@@ -11,11 +14,13 @@ const getPlayerChoice = function () {
   const selection = prompt(
     `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
     ""
-  ).toUpperCase;
+  ).toUpperCase();
+
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
     return DEFAULT_USER_CHOICE;
   }
+  return selection;
 };
 
 const getComputerChoice = function () {
@@ -29,13 +34,29 @@ const getComputerChoice = function () {
   }
 };
 
+const getWinner = function (compChoice, playChoice) {
+  if (compChoice === playChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (compChoice === ROCK && playChoice == PAPER) ||
+    (compChoice === PAPER && playChoice === SCISSORS) ||
+    (compChoice === SCISSORS && playChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
 startGameBtn.addEventListener("click", function () {
   if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
   console.log("Strating game...");
-  const playerSelection = getPlayerChoice;
-  const computerSelection = getComputerChoice;
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+  const winner = getWinner(computerSelection, playerSelection);
   console.log(playerSelection, computerSelection);
+  console.log(winner);
 });
